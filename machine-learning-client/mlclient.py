527763fa3c5377281153c38cf723bb22fe2aa316
dq2024 <dq2024@nyu.edu>
2024-04-13 17:21:40 -0400

    fixed issues with documentation

--- machine-learning-client/mlclient.py
+++ machine-learning-client/mlclient.py
@@ -1,3 +1,4 @@
+"""This module provides functions to convert text from images to audio using IBM Watson."""
 import os
 from dotenv import load_dotenv
 from ibm_watson import TextToSpeechV1
@@ -14,22 +15,39 @@ text_to_speech = TextToSpeechV1(authenticator=authenticator)
 text_to_speech.set_service_url(SERVICE_URL)
 
 def text_from_image(image_path):
+    """Extract text from the specified image file.
 
+    Args:
+        image_path (str): The path to the image file from which to extract text.
+
+    Returns:
+        str or None: The extracted text, or None if the extraction fails.
+    """
     try:
         image = Image.open(image_path)
         text = pytesseract.image_to_string(image)
         return text
-    except Exception as e:
+    except IOError as e:
+        print(f"An error occurred while opening the image: {e}")
+    except Exception as e: 
         print(f"An error occurred while processing the image: {e}")
-        return None
 
 def text_to_audio(text, output_path):
+    """Extract audio from text.
+
+    Args:
+        text: String to be converted to audio
+        output_path (str): The path to the area on local machine where audio file should go
+
+    Returns:
+        None: Prints string if file is uploaded succesfully. 
+    """
     try:
         with open(output_path, 'wb') as audio_file:
             audio_file.write(
                 text_to_speech.synthesize(
                     text,
-                    voice='en-US_AllisonV3Voice',  
+                    voice='en-US_AllisonV3Voice',
                     accept='audio/wav'
                 ).get_result().content
             )
@@ -38,10 +56,10 @@ def text_to_audio(text, output_path):
         print(f"Failed to convert text to speech: {e}")
 
 def main():
-    image_path = 'images/test.jpg'  
-    output_audio_path = 'output_audio.wav'  
+    """Process an image to extract text and convert it to audio."""
+    image_path='images/test.jpg'
+    output_audio_path='output_audio.wav'
 
-    
     text = text_from_image(image_path)
     if text:
         print("Extracted Text:", text)
