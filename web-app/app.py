5a47ba96122124fe7912b862d1f2b59b5bb5d352
Kyoung655 <kay271@nyu.edu>
2024-04-13 19:55:19 -0400

    front-end and app.py

--- web-app/app.py
+++ web-app/app.py
@@ -1,5 +1,4 @@
 from flask import Flask, request
-from markupsafe import escape
 from pymongo import MongoClient
 
 
@@ -9,17 +8,14 @@ app = Flask(__name__)
 # connect to MongoDB server
 client = MongoClient('localhost', 27017)
 
-# get database
-db = client.test
+# # get database
+# db = client.test
 
 @app.route('/index', method=['POST'])
 def get_image():
-    # get image for processing
-    data = request.json
-
-    # put data inside mongoDB 
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
+
 
