b52d7927a030e155450598990e595c6d39288593
tchen0125 <terrancechen2002@gmail.com>
2024-04-14 15:13:41 -0400

    new changes

--- web-app/app.py
+++ web-app/app.py
@@ -2,44 +2,59 @@
 This module contains a Flask web application that processes images.
 """
 import io
+import logging
 from os import getenv
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, abort, flash, redirect, url_for
 from pymongo import MongoClient
 from PIL import Image 
 from bson import binary 
 from dotenv import load_dotenv
 
+logging.basicConfig(level=logging.INFO)
+
 load_dotenv()
 
 app = Flask(__name__)
-client = MongoClient("MONGO_URI") 
+
+mongo_uri = getenv("MONGO_URI")
+client = MongoClient(mongo_uri)
 db = client["imagedb"]
 collection = db["imageCollection"]
+
+app.secret_key = 'your_super_secret_key_here'
 
 @app.route("/", methods=['GET', 'POST'])
 def process_image():
     if request.method == 'POST':
-        print(request.files)
+        logging.info("Received POST request with files: %s", request.files)
         if 'file' not in request.files:
-            return 'No file part'
+            flash('No file part', 'error')
+            return redirect(url_for('process_image'))
+        
         file = request.files['file']
         if file.filename == '':
-            return 'No selected file'
+            flash('No selected file', 'error')
+            return redirect(url_for('process_image'))
+        
         if file:
-            image = Image.open(file.stream)
-            image_byte_array = io.BytesIO()
-            image.save(image_byte_array, format=image.format)
-            image_bytes = image_byte_array.getvalue()
-            # Store image in MongoDB
-            image_document = {
-                'image_data': binary.Binary(image_bytes),
-                'image_name': file.filename
-            }
-            print(client)
-            #collection.insert_one(image_document) #ERROR!!!!!!
-            return 'Image successfully uploaded and added to MongoDB'
+            try:
+                image = Image.open(file.stream)
+                image_byte_array = io.BytesIO()
+                image.save(image_byte_array, format=image.format)
+                image_bytes = image_byte_array.getvalue()
+                
+                image_document = {
+                    'image_data': binary.Binary(image_bytes),
+                    'image_name': file.filename
+                }
+                
+                collection.insert_one(image_document)
+                flash('Image successfully uploaded and added to MongoDB', 'success')
+            except Exception as e:
+                logging.error("An error occurred: %s", e)
+                flash(f'Error processing the image: {e}', 'error')
+
     return render_template("process_image.html")
 
 if __name__ == "__main__":
-    app.run(debug=True)
-
+    app.run(host='0.0.0.0', debug=True)
