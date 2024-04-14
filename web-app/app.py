0dbfd781a11398601addb1e61840680084f6e5ca
Kyoung655 <kay271@nyu.edu>
2024-04-14 05:17:01 -0400

    made mongo connection but has error with collection.insert_one, line 39

--- web-app/app.py
+++ web-app/app.py
@@ -1,26 +1,45 @@
 """
 This module contains a Flask web application that processes images.
 """
-
-from flask import Flask, render_template
+import io
+from os import getenv
+from flask import Flask, request, render_template
 from pymongo import MongoClient
+from PIL import Image 
+from bson import binary 
+from dotenv import load_dotenv
 
+load_dotenv()
 
 app = Flask(__name__)
+client = MongoClient("MONGO_URI") 
+db = client["imagedb"]
+collection = db["imageCollection"]
 
-# connect to MongoDB server
-client = MongoClient("localhost", 27017)
-db = client["sample_mflix"]
-collection = db["images"]
-
-
-@app.route("/", methods=["GET", "POST"])
+@app.route("/", methods=['GET', 'POST'])
 def process_image():
-    """
-    Open the landing page for image uploading
-    """
+    if request.method == 'POST':
+        print(request.files)
+        if 'file' not in request.files:
+            return 'No file part'
+        file = request.files['file']
+        if file.filename == '':
+            return 'No selected file'
+        if file:
+            image = Image.open(file.stream)
+            image_byte_array = io.BytesIO()
+            image.save(image_byte_array, format=image.format)
+            image_bytes = image_byte_array.getvalue()
+            # Store image in MongoDB
+            image_document = {
+                'image_data': binary.Binary(image_bytes),
+                'image_name': file.filename
+            }
+            print(client)
+            #collection.insert_one(image_document) #ERROR!!!!!!
+            return 'Image successfully uploaded and added to MongoDB'
     return render_template("process_image.html")
-
 
 if __name__ == "__main__":
     app.run(debug=True)
+
