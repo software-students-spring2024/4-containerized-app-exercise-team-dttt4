14d9860257efe82cfbc20115149c12838f1c454d
Kyoung655 <kay271@nyu.edu>
2024-04-13 23:44:12 -0400

    templates folder and image upload landing page

--- web-app/app.py
+++ web-app/app.py
@@ -1,26 +1,17 @@
-from flask import Flask, request, render_template
+from flask import Flask, render_template
 from pymongo import MongoClient
-from PIL import Image 
-import io
-from bson import binary 
 
 
 app = Flask(__name__)
-
 
 # connect to MongoDB server
 client = MongoClient('localhost', 27017)
 db = client['sample_mflix']
 collection = db['images']
 
-# # get database
-# db = client.test
-
 @app.route('/', methods=['GET', 'POST'])
 def process_image():
-
     return render_template('process_image.html')
-    #return "<h1>Test</h1>"
 
 
 
