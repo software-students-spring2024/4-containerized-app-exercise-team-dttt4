d6d64737917bc3acab6084573a2ec642a009627f
Kyoung655 <kay271@nyu.edu>
2024-04-14 00:18:41 -0400

    templates folder and image upload landing page

--- web-app/app.py
+++ web-app/app.py
@@ -1,6 +1,7 @@
 """
 This module contains a Flask web application that processes images.
 """
+
 from flask import Flask, render_template
 from pymongo import MongoClient
 
@@ -8,18 +9,18 @@ from pymongo import MongoClient
 app = Flask(__name__)
 
 # connect to MongoDB server
-client = MongoClient('localhost', 27017)
-db = client['sample_mflix']
-collection = db['images']
+client = MongoClient("localhost", 27017)
+db = client["sample_mflix"]
+collection = db["images"]
 
-@app.route('/', methods=['GET', 'POST'])
+
+@app.route("/", methods=["GET", "POST"])
 def process_image():
     """
     Open the landing page for image uploading
     """
-    return render_template('process_image.html')
+    return render_template("process_image.html")
 
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
