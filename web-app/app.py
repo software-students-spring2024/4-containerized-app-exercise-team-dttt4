c21039bfda999ccc2ad30f2f4752e7e84d0438da
dq2024 <dq2024@nyu.edu>
2024-04-15 00:19:16 -0400

    documentation changes

--- web-app/app.py
+++ web-app/app.py
@@ -1,10 +1,12 @@
+"""App for handling image uploads and initiating image processing."""
+
 import io
 import logging
 from os import getenv
-from flask import Flask, request, render_template, flash, redirect, url_for, jsonify
+from flask import Flask, request, render_template, flash, redirect, url_for
 from pymongo import MongoClient
 from gridfs import GridFS
-from bson import binary, ObjectId
+from bson import binary
 from dotenv import load_dotenv
 import requests
 
@@ -19,16 +21,17 @@ mongo_uri = getenv("MONGO_URI")
 client = MongoClient(mongo_uri)
 db = client["imagedb"]
 collection = db["imageCollection"]
-fs = GridFS(db) 
+fs = GridFS(db)
 
 @app.route("/", methods=["GET", "POST"])
 def upload_image():
+    """Handle image upload requests from users."""
     if request.method == "POST":
         file = request.files.get("file")
         if file and file.filename:
             try:
                 image_byte_array = io.BytesIO()
-                file.save(image_byte_array)  # Save the image to byte array
+                file.save(image_byte_array)  
                 image_bytes = image_byte_array.getvalue()
 
                 image_document = {
@@ -47,14 +50,14 @@ def upload_image():
 
 @app.route("/list_text")
 def list_text():
-    # Fetch only processed documents where text is available
+    """Display list of processed images with their extracted text."""
     text_documents = collection.find({"is_processed": True}, {"text": 1, "image_name": 1})
     return render_template("list_text.html", text_documents=text_documents)
 
 @app.route('/trigger_process', methods=['POST'])
 def trigger_process():
+    """Trigger the image processing service."""
     try:
-        # URL of the mlclient.py process route
         mlclient_url = "http://mlclient:5001/process"
         response = requests.post(mlclient_url)
         if response.status_code == 200:
